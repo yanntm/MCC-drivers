7b6870bd96270627c31b9ab86aeeeb62d187bd47
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2023-02-06 14:33:50 +0100

    more packages : java, unzip, psmisc
    unzip to decompress its-tools
    java to run it
    psmisc for killall, to kill it

--- pnmc/install.sh
+++ pnmc/install.sh
@@ -3,7 +3,7 @@
 # install as root
 su
 apt-get update
-apt-get install git libboost-dev g++ ca-certificates python
+apt-get install git libboost-dev g++ ca-certificates python3 unzip openjdk-17-jdk psmisc
 
 exit
 
@@ -14,3 +14,5 @@ git clone -depth 1 https://github.com/yanntm/MCC-drivers.git
 
 cp -r MCC-drivers/pnmc/* .
 
+# for unfolding
+./install_itstools.sh
