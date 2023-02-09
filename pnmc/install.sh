62a3de1bc27a1f0cc8a969f354e5d5cb6c75c2a6
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2023-02-09 13:42:44 +0100

    separate scripts for setting up packages from install script

--- pnmc/install.sh
+++ pnmc/install.sh
@@ -1,12 +1,5 @@
 #! /bin/sh
 
-# install as root
-su
-apt-get update
-apt-get install git libboost-dev g++ ca-certificates python3 unzip openjdk-17-jdk psmisc
-
-exit
-
 cd 
 cd BenchKit
 
---
+++ pnmc/install_packages.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+# install as root
+su
+apt-get update
+apt-get install git ca-certificates python3 unzip openjdk-17-jdk psmisc
+
