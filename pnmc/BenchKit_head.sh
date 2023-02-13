6efe1ba1159886968b420e7d39103af949f2f1de
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2023-02-13 17:43:14 +0100

    avoid stack overflow issues ; we need a *lot* of recursion

--- pnmc/BenchKit_head.sh
+++ pnmc/BenchKit_head.sh
@@ -11,4 +11,8 @@
 # BK_EXAMINATION: it is a string that identifies your "examination"
 
 export PATH="$PATH:$BK_BIN_PATH"
+
+# pnmc may use a lot of recursion stack, this is normal.
+ulimit -s 65536
+
 $BK_BIN_PATH/pnmc.py
