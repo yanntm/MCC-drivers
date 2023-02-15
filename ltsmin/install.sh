3cf796cfb632930515b5bf3d7a0220ddede33e13
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2023-02-15 16:22:13 +0100

    grab binaries from LTSMin-BinaryBuilds

---
+++ ltsmin/install.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+cd bin ;  
+mkdir dl
+cd dl
+wget https://github.com/yanntm/LTSmin-BinaryBuilds/raw/gh-pages/ltsmin_linux_64.tar.gz
+tar xvzf ltsmin_linux_64.tar.gz
+mv lts_install_dir/bin/pnml* ..
+cd ..
+rm -rf dl/
+
+# for unfolding
+./install_itstools.sh
