33c336c54168c5bc3f337114d122d75c8a50b0dd
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2023-02-15 16:22:27 +0100

    simplify script

--- pnmc/install.sh
+++ pnmc/install.sh
@@ -1,11 +1,6 @@
 #! /bin/sh
 
-cd 
-cd BenchKit
 
-git clone --depth 1 https://github.com/yanntm/MCC-drivers.git
-
-cp -r MCC-drivers/pnmc/* .
 cd bin ; wget https://github.com/yanntm/pnmc/raw/gh-pages/pnmc ; chmod a+x pnmc ; wget https://github.com/yanntm/caesar.sdd/raw/gh-pages/caesar.sdd ; chmod a+x caesar.sdd ; cd ..
 
 # for unfolding
