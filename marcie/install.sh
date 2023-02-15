7b468ceb7c44df29a5b46cbe684fc4a72b4bbf6b
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2023-02-15 12:34:44 +0100

    script to download marcie and deploy to bin/

---
+++ marcie/install.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+# grab archive
+wget --no-check-certificate https://www-dssz.informatik.tu-cottbus.de/track/download.php?id=240 -O marcie.zip
+
+# extract marcie
+unzip -j marcie.zip marcie-linux64-20191118/bin/marcie -d .
+
+mv marcie bin/
+
