95927806312f01316f11fbc23070e43b9ea04fa3
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2023-02-23 16:58:20 +0100

    A reducer tool

---
+++ reducer/BenchKit_head.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+echo "Invoking reducer"
+
+export BINDIR=$BK_BIN_PATH/../../itstools/bin/
+export MODEL=$(pwd)
+
+$BINDIR/runeclipse.sh $MODEL $BK_EXAMINATION -timeout $BK_TIME_CONFINEMENT -rebuildPNML
+
+if [ -f model.sr.pnml ] 
+then
+	mkdir $$
+	cp $BK_EXAMINATION.sr.xml $$/$BK_EXAMINATION.xml
+	cp model.sr.pnml $$/model.pnml
+	cd $$
+
+	export BK_BIN_PATH=$BK_BIN_PATH/../../$BK_TOOL/bin
+	bash $BK_BIN_PATH/../BenchKit_head.sh
+
+	cd ..
+fi
