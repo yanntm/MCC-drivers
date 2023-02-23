e87d1be044a552877ef3dc1479b06c12692de70b
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2023-02-23 17:04:53 +0100

    wrong relative path

--- reducer/BenchKit_head.sh
+++ reducer/BenchKit_head.sh
@@ -6,7 +6,7 @@ echo "Invoking reducer"
 export BINDIR=$BK_BIN_PATH/../../itstools/bin/
 export MODEL=$(pwd)
 
-$BINDIR/runeclipse.sh $MODEL $BK_EXAMINATION -timeout $BK_TIME_CONFINEMENT -rebuildPNML
+$BINDIR/../runeclipse.sh $MODEL $BK_EXAMINATION -timeout $BK_TIME_CONFINEMENT -rebuildPNML
 
 if [ -f model.sr.pnml ] 
 then
