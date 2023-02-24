e661635126defd3cb0fe4a44ad1c0b6acf136ec8
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2023-02-24 10:53:28 +0100

    always run the next tool unless it's over.

--- reducer/BenchKit_head.sh
+++ reducer/BenchKit_head.sh
@@ -8,15 +8,20 @@ export MODEL=$(pwd)
 
 $BINDIR/runeclipse.sh $MODEL $BK_EXAMINATION -timeout $BK_TIME_CONFINEMENT -rebuildPNML
 
+# prep next tool
+export BK_BIN_PATH=$BK_BIN_PATH/../../$BK_TOOL/bin
+
 if [ -f model.sr.pnml ] 
 then
+	# there are residual problems
 	mkdir $$
 	cp $BK_EXAMINATION.sr.xml $$/$BK_EXAMINATION.xml
 	cp model.sr.pnml $$/model.pnml
 	cd $$
 
-	export BK_BIN_PATH=$BK_BIN_PATH/../../$BK_TOOL/bin
 	bash $BK_BIN_PATH/../BenchKit_head.sh
 
 	cd ..
+else
+	bash $BK_BIN_PATH/../BenchKit_head.sh
 fi
