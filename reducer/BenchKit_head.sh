f213d532898916573c3f58ecdc37c5250fe35e3d
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2023-04-25 14:06:22 +0200

    Change Fireability to Cardinality since ITS will degenerate them

--- reducer/BenchKit_head.sh
+++ reducer/BenchKit_head.sh
@@ -21,10 +21,13 @@ if [ -f model.sr.pnml ]
 then
 	echo "There are residual formulas that ITS could not solve within timeout"
 	mkdir $$
-	cp $BK_EXAMINATION.sr.xml $$/$BK_EXAMINATION.xml
+	export BK2=$(echo $BK_EXAMINATION | sed 's/Fireability/Cardinality/g') ;
+	cp $BK_EXAMINATION.sr.xml $$/$BK2.xml
 	cp model.sr.pnml $$/model.pnml
 	cd $$
-
+	
+	
+	export BK_EXAMINATION=$BK2 ;
 	bash $BK_BIN_PATH/../BenchKit_head.sh
 
 	cd ..
