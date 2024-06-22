bee4cb67bcd1c069c9740f57570d83f094f433e6
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2024-06-22 13:12:53 +0200

    more sh tweaking

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/sh
 
 #./install_itstools.sh
 
@@ -8,17 +8,15 @@ for i in */ ;
 do
 	cd $i
 	if [ -f  install.sh ] ; then 
+	  echo "Installing tool : $i"
 		./install.sh
 	fi
 	cd ..
 done
 
 
-if [ -f ~/.profile ] ; then
 grep "timeout" ~/.profile > /dev/null
 if [ $? != 0 ]; then
 	echo "alias timeout=$PWD/bin/timeout.pl" >> ~/.profile
 	echo "shopt -s expand_aliases" >> ~/.profile
 fi
-source ~/.profile
-fi 
