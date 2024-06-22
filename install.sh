f0616423496467d906f95a29f9e99ed4b5a0514a
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2024-06-22 12:54:30 +0200

    skip profile in Dockers context

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 #./install_itstools.sh
 
+set -x
+
 for i in */ ; 
 do
 	cd $i
@@ -12,10 +14,11 @@ do
 done
 
 
+if [ -f ~/.profile ] ; then
 grep "timeout" ~/.profile > /dev/null
 if [ $? != 0 ]; then
 	echo "alias timeout=$PWD/bin/timeout.pl" >> ~/.profile
 	echo "shopt -s expand_aliases" >> ~/.profile
 fi
 source ~/.profile
- 
+fi 
