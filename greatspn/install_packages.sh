8a929e1c5e31e1cf0663675befbd70b5e6e68987
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2025-05-12 15:38:45 +0200

    jdk 21

--- greatspn/install_packages.sh
+++ greatspn/install_packages.sh
@@ -4,7 +4,7 @@
 apt-get update
 
 # java, some dev libs
-apt-get -y install unzip openjdk-17-jdk 
+apt-get -y install unzip openjdk-21-jdk 
 
 # note that GreatSPN also needs :
 # Spot (libspot.so) and related Buddy
